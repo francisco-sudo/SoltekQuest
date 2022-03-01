9ae9f941aaeb5c193e7e75abb991695e63d1f435
francisco-sudo <82182554+francisco-sudo@users.noreply.github.com>
2022-03-01 10:46:25 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+const btnSignIn = document.querySelector(".sign-in-btn"),
+    btnSignUp = document.querySelector(".sign-up-btn"),
+    signUp = document.querySelector(".sign-up"),
+    signIn = document.querySelector(".sign-in"),
+    btnForgotPass = document.querySelector(".forgot-password-btn"),
+    btnSignIn2 = document.querySelector(".sign-in-btn-2"),
+    forgotPass = document.querySelector(".forgot-password");
+
+
+navToggle.addEventListener("click", () =>{
+    navMenu.classList.toggle("nav-menu-visible");
+});
+
+document.addEventListener("click", e =>{
+    if(e.target === btnSignIn || e.target === btnSignUp){
+        signIn.classList.toggle("active");
+        signUp.classList.toggle("active");
+    }
+});
+
+document.addEventListener("click", e =>{
+    if(e.target === btnForgotPass || e.target === btnSignIn2){
+        signIn.classList.toggle("active");
+        forgotPass.classList.toggle("active");
+    }
+});
